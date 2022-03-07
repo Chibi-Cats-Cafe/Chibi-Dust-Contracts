fb3953cb60829830b3be0c1cb73e05f8fa71f1c7
AceVikings <akash.jsmith@gmail.com>
2022-03-07 13:01:24 +0530

    Update ChibiTeamVesting.sol

---
+++ contracts/ChibiTeamVesting.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ChibiDustICO is Ownable{
+
+    IERC20 ChibiDust;
+
+    struct members{
+        address memberAddress;
+        uint share;
+    }
+    
+    members[5] public teamMembers;
+    uint[] unlockTime = [1646611200,1649289600,1651881600,1654560000,1657152000,1659830400,1662508800,1665100800];
+    uint TOTAL = 15_000_000;
+    uint8 countsRetrieved;
+
+    constructor(address _dust,members[5] memory team){
+        ChibiDust = IERC20(_dust);
+        teamMembers = team;
+    }
+
+    function retrieveSalary() external onlyOwner{
+        uint amountToSplit = TOTAL*125/1000;
+        require(block.timestamp > unlockTime[countsRetrieved],"Not yet unlocked");
+        countsRetrieved++;
+        for(uint i = 0; i < 5; i++){
+            address member = teamMembers[i].memberAddress;
+            uint share = teamMembers[i].share;
+            ChibiDust.transfer(member,amountToSplit*share/1000);
+        }
+    }
+ 
+}
